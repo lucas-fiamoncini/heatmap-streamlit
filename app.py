743edd4222ab26bae097554e667b8303fec8c141
Lucas Fiamoncini <lucas.fiamoncini00@gmail.com>
2025-03-17 11:09:14 -0300

    debug

--- app.py
+++ app.py
@@ -93,11 +93,15 @@ def main():
         heatmap_colored = cv2.applyColorMap((heatmap_inverted * 255).astype(np.uint8), cv2.COLORMAP_JET)
         blended = cv2.addWeighted(display_img, 0.5, heatmap_colored, 0.5, 0)
 
+        st.error("Blended image shape:", blended.shape)
+        st.error("Blended image data type:", blended.dtype)
+
+        st.image(blended, use_column_width=True, caption="Draw on the image to create a heatmap")
+
         canvas = st_canvas(
             stroke_width=st.session_state.brush_size,
             stroke_color="#e4eded",
-            # background_image=Image.fromarray(blended),
-            background_image=blended,
+            background_image=Image.fromarray(blended),
             height=blended.shape[0],
             width=blended.shape[1],
             drawing_mode="freedraw",
